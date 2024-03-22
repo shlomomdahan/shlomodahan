b46d7997e80a412419789d99728ddf1f8fee2e8c
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-03-21 21:59:55 -0400

    fix responsiveness

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -26,7 +26,7 @@ export const Projects = () => {
   const isExpanded = visibleProjects === projectsData.length;
 
   return (
-    <div className="w-full p-1 mx-5 mb-20">
+    <div className="w-full p-1 mb-20">
       <h1 className="text-3xl font-bold mb-5">Projects</h1>
       <Separator />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3.5xl:grid-cols-4 gap-10">
