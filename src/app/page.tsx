01e8dc2f3a1663208bdb4da9162e87fc1b347254
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-03-21 20:33:28 -0400

    fix responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,20 @@ import { Intro } from "@/components/Intro";
 import { Projects } from "@/components/Projects";
 import { Experiences } from "@/components/Experiences";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    // <main className="min-h-screen py-10 px-20 bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#7fa2db_100%)]">
-    <main className="min-h-screen py-10 px-20 absolute top-0 z-[-2] bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,#c3dbfa_100%)]">
-      <Intro />
-      <Projects />
-      <Experiences />
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className="min-h-screen py-10 px-20 absolute top-0 z-[-2] bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,#c3dbfa_100%)]">
+        <Intro />
+        <Projects />
+        <Experiences />
+        <Footer />
+      </main>
+    </>
   );
 }
