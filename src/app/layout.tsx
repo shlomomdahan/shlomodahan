58c42e2ebf3296719152c80fd0956479ca9c6393
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-03-21 20:05:10 -0400

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +17,10 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
